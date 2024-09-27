cc2049dfc29459186d7ac5e49625700f3ff26e6f
thesahilkamble <sahil402424@gmail.com>
2024-09-27 15:40:03 +0530

    Task List Component added

---
+++ To-do frontend/todo-app/src/components/taskList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TaskList = ({ tasks, onEdit, onDelete }) => {
+return(
+    <div>
+    <h2>
+        Task List
+        </h2>
+        <ul>
+            {tasks.map((task) => ( 
+                <li key={task.id}>
+                    {task.title}
+                    <button onClick={() => onEdit(task.id)}>Edit</button>
+                    <button onClick={() => onDelete(task.id)}>Delete</button>
+                </li>
+            ))}
+        </ul>
+        </div>
+)
+}
